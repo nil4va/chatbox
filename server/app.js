3e0be160fcd2268912dd2649570d49624f54bf3c
Alfa Saijers <alfa@devo.pw>
2021-03-03 14:04:16 +0100

    get gateway route

--- server/app.js
+++ server/app.js
@@ -3,87 +3,100 @@
  *
  * @author Pim Meijer
  */
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const db = require("./utils/databaseHelper");
-const cryptoHelper = require("./utils/cryptoHelper");
-const corsConfig = require("./utils/corsConfigHelper");
-const app = express();
-const fileUpload = require("express-fileupload");
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const db = require('./utils/databaseHelper')
+const cryptoHelper = require('./utils/cryptoHelper')
+const corsConfig = require('./utils/corsConfigHelper')
+const app = express()
+const fileUpload = require('express-fileupload')
 
 //logger lib  - 'short' is basic logging info
-app.use(morgan("short"));
+app.use(morgan('short'))
 
 //init mysql connectionpool
-const connectionPool = db.init();
+const connectionPool = db.init()
 
 //parsing request bodies from json to javascript objects
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //CORS config - Cross Origin Requests
-app.use(corsConfig);
+app.use(corsConfig)
 
 //File uploads
-app.use(fileUpload());
+app.use(fileUpload())
 
 // ------ ROUTES - add all api endpoints here ------
-const httpOkCode = 200;
-const badRequestCode = 400;
-const authorizationErrCode = 401;
+const httpOkCode = 200
+const badRequestCode = 400
+const authorizationErrCode = 401
 
-app.post("/user/login", (req, res) => {
-    const username = req.body.username;
+app.post('/user/login', (req, res) => {
+  const username = req.body.username
 
-    //TODO: We shouldn't save a password unencrypted!! Improve this by using cryptoHelper :)
-    const password = req.body.password;
+  //TODO: We shouldn't save a password unencrypted!! Improve this by using cryptoHelper :)
+  const password = req.body.password
 
-    db.handleQuery(connectionPool, {
-        query: "SELECT username, password FROM user WHERE username = ? AND password = ?",
-        values: [username, password]
-    }, (data) => {
-        if (data.length === 1) {
-            //return just the username for now, never send password back!
-            res.status(httpOkCode).json({"username": data[0].username});
-        } else {
-            //wrong username
-            res.status(authorizationErrCode).json({reason: "Wrong username or password"});
-        }
-
-    }, (err) => res.status(badRequestCode).json({reason: err}));
-});
+  db.handleQuery(
+    connectionPool,
+    {
+      query:
+        'SELECT username, password FROM user WHERE username = ? AND password = ?',
+      values: [username, password],
+    },
+    data => {
+      if (data.length === 1) {
+        //return just the username for now, never send password back!
+        res.status(httpOkCode).json({ username: data[0].username })
+      } else {
+        //wrong username
+        res
+          .status(authorizationErrCode)
+          .json({ reason: 'Wrong username or password' })
+      }
+    },
+    err => res.status(badRequestCode).json({ reason: err })
+  )
+})
 
 //dummy data example - rooms
-app.post("/room_example", (req, res) => {
+app.post('/room_example', (req, res) => {
+  db.handleQuery(
+    connectionPool,
+    {
+      query: 'SELECT id, surface FROM room_example WHERE id = ?',
+      values: [req.body.id],
+    },
+    data => {
+      //just give all data back as json
+      res.status(httpOkCode).json(data)
+    },
+    err => res.status(badRequestCode).json({ reason: err })
+  )
+})
 
-    db.handleQuery(connectionPool, {
-            query: "SELECT id, surface FROM room_example WHERE id = ?",
-            values: [req.body.id]
-        }, (data) => {
-            //just give all data back as json
-            res.status(httpOkCode).json(data);
-        }, (err) => res.status(badRequestCode).json({reason: err})
-    );
+app.post('/upload', function (req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res
+      .status(badRequestCode)
+      .json({ reason: 'No files were uploaded.' })
+  }
 
-});
+  let sampleFile = req.files.sampleFile
 
-app.post("/upload", function (req, res) {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(badRequestCode).json({ reason: "No files were uploaded." });
+  sampleFile.mv(wwwrootPath + '/uploads/test.jpg', function (err) {
+    if (err) {
+      return res.status(badRequestCode).json({ reason: err })
     }
 
-    let sampleFile = req.files.sampleFile;
-
-    sampleFile.mv(wwwrootPath + "/uploads/test.jpg", function (err) {
-        if (err) {
-            return res.status(badRequestCode).json({ reason: err });
-        }
-
-        return res.status(httpOkCode).json("OK");
-    });
-});
+    return res.status(httpOkCode).json('OK')
+  })
+})
 //------- END ROUTES -------
 
-module.exports = app;
-
+app.get('/gateway', (req, res) => {
+  res.send(httpOkCode).json({ gateway: '/ws' })
+})
+module.exports = app
