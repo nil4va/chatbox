7c4d13c1a9147161fedfbe8aa70f4d10a2bf6105
Alfa Saijers <alfa@devo.pw>
2021-03-04 13:33:24 +0100

    refactor

--- src/assets/js/controllers/chatController.js
+++ src/assets/js/controllers/chatController.js
@@ -1,24 +1,40 @@
-import { html, render } from '../../vendors/preact/preact.js'
-import Chat from '../components/Chat.js'
 import ChatRepository from '../repositories/chatRepository.js'
-import { qs } from '../utils/alfa.js'
+import { ce, qs } from '../utils/alfa.js'
 
 class ChatController {
   constructor() {
     this.chatRepository = new ChatRepository('test', 'server')
-    qs('.content').innerHTML = ''
-    this.render()
-    this.chatRepository.on('message', e => {
-      this.render()
-    })
+    this.init()
   }
-  render() {
-    render(
-      html`
-        <${Chat} chatRepository=${this.chatRepository} />
-      `,
-      qs('.content')
-    )
+
+  async init() {
+    let res = await fetch('views/chat.html')
+    let html = await res.text()
+    console.log(html)
+    qs('.content').innerHTML = html
+    this.showMessages()
+    this.chatRepository.on('message', e => {
+      this.showMessages()
+    })
+    qs('#msgsend').onclick = e =>
+      this.chatRepository.send(qs('#msginput').value)
+  }
+
+  showMessages() {
+    // render all the messages
+    qs('.history').innerHTML = ''
+    this.chatRepository.getAll().map(msg => {
+      qs('.history').append(
+        ce('div', {
+          className:
+            'msg ' +
+            (msg.from === this.chatRepository.getFrom()
+              ? 'msgself'
+              : 'msgother'),
+          innerHTML: `<p>${msg.content}</p>`,
+        })
+      )
+    })
   }
 }
 
