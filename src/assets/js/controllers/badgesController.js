20e9d620fedf3d2c0fcb1981b3cc5e185834d453
Nilava Kazal <nilava.kazal@hva.nl>
2021-05-12 08:55:00 +0200

    page for all badge info and if they are earned or not

--- src/assets/js/controllers/badgesController.js
+++ src/assets/js/controllers/badgesController.js
@@ -5,16 +5,30 @@
 
 class BadgesController {
     constructor() {
+        this.badgeRepository = new badgeRepository();
+
         $.get('views/badges.html')
             .done(htmlData => this.setup(htmlData))
             .fail(() => this.error())
     }
 
-    setup(htmlData) {
+    async setup(htmlData) {
         this.badgesView = $(htmlData);
-
-// toevoegen html aan .content div
         $(".content").empty().append(this.badgesView);
+
+        const earnedBadges = await this.badgeRepository.getBadgeInfo(sessionManager.get('username'))
+
+        console.log(earnedBadges)
+
+        for (let i = 0; i < earnedBadges.length; i++) {
+            $('#badge' + (i + 1)).hide()
+        }
+
+        for (let i = 0; i < 4; i++) {
+            if ( typeof earnedBadges[i] === "undefined" ) {
+                $('.badge' + (i + 1)).css("opacity", "50%")
+            }
+        }
     }
 
     error() {
